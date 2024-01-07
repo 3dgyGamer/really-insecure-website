e1dd687bbcb42683dc5f5c9ce1f102fa1830eec8
3dgyGamer <jakepark2908@gmail.com>
2024-01-07 00:44:58 -0500

    starting up

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"regexp"
+	"syscall"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func web(){
+	
+}
+
+func main(){
+
+}
